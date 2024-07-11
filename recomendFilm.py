5d270917f0874eef6cff4f7d361ac53940897b99
Misha2502 <yegrashich@gmail.com>
2024-07-11 22:36:14 +0300

    new funcs, list of films and smarter recomendations?

--- recomendFilm.py
+++ recomendFilm.py
@@ -4,12 +4,16 @@ import math
 #detective, romantic, action, horror
 romantik = [3, 5, 2, 1] 
 matrix = [4, 2, 5, 2]
+theRing = [3, 1, 2, 5]
+sherlock = [5, 2, 4, 2]
 
+neighbors = [romantik, matrix, theRing, sherlock]
+favorite_film = ["Titanik", "Matrix", "The Ring", "Sherlock"]
 
 chloe = [3, 5, 3, 1]
 jhon = [4, 3, 5, 3]
 
-def lengh_of_neighbor(first_neighbor, second_neighbor):
+def lengh_of_neighbors(first_neighbor, second_neighbor):
     sum = 0
     for i in range(len(first_neighbor)):
         lengh = (first_neighbor[i] - second_neighbor[i])**2
@@ -17,11 +21,25 @@ def lengh_of_neighbor(first_neighbor, second_neighbor):
 
     return int(math.sqrt(sum))
 
-def recomend_film(some):
-    if lengh_of_neighbor(romantik, some) < lengh_of_neighbor(matrix, some):
-        print("Let`s watch Titanik!")
-    if lengh_of_neighbor(romantik, some) > lengh_of_neighbor(matrix, some):
-        print("Let`s watch Matrix!")
+def recomend_film(client):
+    index = recomend_from_neighbors(client)
+    if index != None:
+        return favorite_film[index]
+    
 
-recomend_film(chloe)
-recomend_film(jhon)
+    
+
+def recomend_from_neighbors(client):
+        best_neighbor = 1000
+        index_of_neighbor = None
+        for i in range(4):
+            recomend_index = lengh_of_neighbors(client, neighbors[i])
+            if  recomend_index < best_neighbor:
+                best_neighbor = recomend_index
+                index_of_neighbor = i
+        return(index_of_neighbor)
+
+
+
+print(recomend_film(chloe))
+print(recomend_film(jhon))
